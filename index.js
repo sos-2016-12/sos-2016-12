1540f4ee3cc45f976359834dfa9e156f643ea90f
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-02-29 22:57:08 +0100

    First version

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/",(req,res) => {
+
+	res.write("Group Members: \n");
+	res.write(" - Alejandro Miguel Ruiz Rodriguez \n");
+	res.write(" - Victor Alejandro Segura Segura \n");
+	res.write("Our app is a representation of the use of Death Penalty \n and what countries switched to a Democratic Government in the world. \n");
+	res.end();
+
+})
+
+app.listen(16666);
