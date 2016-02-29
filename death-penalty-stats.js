7ff15335ea492dde368566cbff5799b72de24807
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-03-01 00:00:20 +0100

    adjusting script for it to work

--- death-penalty-stats.js
+++ death-penalty-stats.js
@@ -5,7 +5,7 @@ var app = express();
 app.get("/about/death-penalty-stats",(req,res) => {
 
 	res.write("\n");
-	res.write(" <html><body><a src="http://www.ub.edu/penal/historia/PdeM/listapaises.htm">death-penalty-stats</a></body></html> \n");
+	res.write(" <html><body><a href="http://www.ub.edu/penal/historia/PdeM/listapaises.htm">death-penalty-stats</a></body></html> \n");
 	res.end();
 
 })
