665afdcf682da0439b86851550bf5fe20d45ee22
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-03-01 01:16:41 +0100

    Cambios esteticos

--- about.js
+++ about.js
@@ -15,8 +15,8 @@ app.get("/about",(req,res) => {
 app.get("/about/death-penalty-stats",(req,res) => {
 
 	res.write("\n");
-	res.write("Death penalty abolition in different countries: http://www.ub.edu/penal/historia/PdeM/listapaises.htm \n");
-	res.write("Homicide rate in the world: https://en.wikipedia.org/wiki/List_of_countries_by_intentional_homicide_rate \n");
+	res.write(" - Death penalty abolition in different countries: http://www.ub.edu/penal/historia/PdeM/listapaises.htm \n");
+	res.write(" - Homicide rate in the world: https://en.wikipedia.org/wiki/List_of_countries_by_intentional_homicide_rate \n");
 	res.end();
 
 })
@@ -24,9 +24,9 @@ app.get("/about/death-penalty-stats",(req,res) => {
 app.get("/about/republican-stats",(req,res) => {
 
 	res.write("\n");
-	res.write("List of countries by date of transition to republic: https://en.wikipedia.org/wiki/List_of_countries_by_date_of_transition_to_republican_system_of_government \n");
-	res.write("GDP of countries per capita: https://es.wikipedia.org/wiki/Anexo:Pa%C3%ADses_por_PIB_(PPA)_per_c%C3%A1pita \n");
-	res.write("Population of countries: https://es.wikipedia.org/wiki/Anexo:Pa%C3%ADses_por_poblaci%C3%B3n \n");
+	res.write(" - List of countries by date of transition to republic: https://en.wikipedia.org/wiki/List_of_countries_by_date_of_transition_to_republican_system_of_government \n");
+	res.write(" - GDP of countries per capita: https://es.wikipedia.org/wiki/Anexo:Pa%C3%ADses_por_PIB_(PPA)_per_c%C3%A1pita \n");
+	res.write(" - Population of countries: https://es.wikipedia.org/wiki/Anexo:Pa%C3%ADses_por_poblaci%C3%B3n \n");
 	res.end();
 
 })
