14a90a42087a8978d8b5dcbef04348c7dc803cb2
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-03-03 20:13:26 +0100

    Added teammate's source

--- about.js
+++ about.js
@@ -7,7 +7,8 @@ app.get("/about",(req,res) => {
 	res.write("Group Members: \n");
 	res.write(" - Alejandro Miguel Ruiz Rodriguez \n");
 	res.write(" - Victor Alejandro Segura Segura \n \n");
-	res.write(" Our app is a representation of the use of Death Penalty \n and what countries switched to a Democratic Government in the world. \n");
+	res.write(" - Matthias Rein \n \n");
+	res.write(" Our app is a representation of the use of Death Penalty \n ,what countries switched to a Democratic Government \n and the effects of earthquakes in different countries of the world. \n");
 	res.end();
 
 })
