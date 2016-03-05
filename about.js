298350a9ea4398628a041fda47dbadcf2d55719f
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-03-06 00:36:15 +0100

    using the static directive for serving the content

--- about.js
+++ about.js
@@ -2,6 +2,6 @@ var express = require("express");
 
 var app = express();
 
-app.use("/about",express.static(__dirname+"/static"));
+app.use("/",express.static(__dirname+"/static"));
 
 app.listen(process.env.PORT);
