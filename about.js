b042fe0efc88cdf3bb1007a497e2799ae8289db1
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-03-06 01:28:41 +0100

    using the static directive for serving the content

--- about.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-
-var app = express();
-
-app.use("/about",express.static(__dirname+"/static"));
-
-app.listen(process.env.PORT);
---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+app.use("/",express.static(__dirname+"/static"));
+
+var p = (process.env.PORT || 16666);
+
+app.get("/time",(req,res) => {
+
+	var now = new Date();
+	res.write("It is "+now);
+	res.end();
+
+})
+
+app.listen(p);
