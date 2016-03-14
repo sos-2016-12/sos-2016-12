9784210a84631c97bb990864f8428cdd1af1cbb3
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-03-15 00:11:07 +0100

    Setting up API

--- index.js
+++ index.js
@@ -13,4 +13,23 @@ app.get("/time",(req,res) => {
 	res.end();
 })
 
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+var games = [ { name : ""}];
+
+app.get("/api/sandbox/games/:name", (req,res) =>{
+	var name = req.params.name;
+	console.log("New GET of resource "+name);
+	res.send(games[name]);
+});
+
+app.post("/api/sandbox/games", (req,res) =>{
+	var game = req.body;
+	games.push(contact);
+	console.log("New POST of resource "+game.name);
+	res.sendStatus(200);
+})
+
 app.listen(p);    
