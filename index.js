94b8a5702da3d7aa5c245bc20bbd01c26cc48b9a
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-03-29 16:39:31 +0200

    Implementing API methods for my source

--- index.js
+++ index.js
@@ -128,13 +128,23 @@ app.get("/api/v1/death_penalty_stats", (req,res) =>{
 
 app.get("/api/v1/death_penalty_stats/:country", (req,res) =>{
 	var country = req.params.country;
-	var aux = null;
-	for (var i = 0; i < death_penalty_stats.length; i++) {
-		if (death_penalty_stats[i].country == country){
-			aux = death_penalty_stats[i];
-			res.send(aux);
+	var aux = [];
+	if (isNaN(country)) {
+		for (var i = 0; i < death_penalty_stats.length; i++) {
+			if (death_penalty_stats[i].country == country){
+				aux.push(death_penalty_stats[i]);
+				res.send(aux);
+			}
+		}
+	}else {
+		for (var i = 0; i < death_penalty_stats.length; i++) {
+			if (death_penalty_stats[i].abolition_year == country){
+				aux.push(death_penalty_stats[i]);
+				res.send(aux);
+			}
 		}
 	}
+		
 	if (aux == null) {
 		res.sendStatus(404);
 	}
