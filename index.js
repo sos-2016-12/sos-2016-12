48e6bb49ca5f631d8b6551666fd1d24e346a876e
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-03-29 03:03:12 +0200

    Implementing API methods for my source

--- index.js
+++ index.js
@@ -144,8 +144,9 @@ app.get("/api/v1/death_penalty_stats/:country", (req,res) =>{
 
 app.post("/api/v1/death_penalty_stats", (req,res) =>{
 	var stat = req.body;
-	if (stat.country == null || stat.country == "" || (typeof stat.country) != string || stat.abolition_year.isNaN() 
-		|| (stat.for_all_crimes != "yes" && stat.for_all_crimes != "no") || stat.murder_rate_per_100k_people.isNaN() ) {
+	if (stat.country == null || stat.country == "" || !stat.country.isString() || stat.abolition_year.isNan()
+		|| (stat.for_all_crimes != "yes" && stat.for_all_crimes != "no") || stat.murder_rate_per_100k_people.isNaN() || stat.murder_rate_per_100k_people == null 
+		|| stat.murder_rate_per_100k_people == "" ) {
 		res.sendStatus(409);
 	} else {
 		death_penalty_stats.push(stat);
