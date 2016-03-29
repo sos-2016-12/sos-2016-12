8c81b8ee27df112da873a3a1f8099e5ff167fc7e
Victor <longjohnsilver@openmailbox.org>
2016-03-29 13:26:14 +0100

    fin11

--- index.js
+++ index.js
@@ -253,7 +253,7 @@ app.get("/api/v1/republican_stats/", (req,res) =>{
 	var to = req.query.to;
 	var aux = null;
 	for (var i = 0; i < republican_stats.length; i++) {
-		if (republican_stats[i].year >= fromm || republican_stats[i].year <= to){
+		if (republican_stats[i].year >= fromm && republican_stats[i].year <= to){
 			aux = aux + republican_stats[i];
 		}
 	}
