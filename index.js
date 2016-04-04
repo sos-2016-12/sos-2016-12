e61f62e2312714f7b91cf8368e8eefbbadb76cf7
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-04-04 22:55:22 +0200

    Implementing API methods for my source

--- index.js
+++ index.js
@@ -122,21 +122,21 @@ app.get("/api/v1/death_penalty_stats/loadInitialData", (req,res) =>{
 });
 
 app.get("/api/v1/death_penalty_stats", (req,res) =>{
-	var limit = req.query.limit;
-	var offset = req.query.offset;
-	var auxList = []
-	if (isNaN(offset)){
-		offset = 0;
-	}
-	if (!isNaN(limit)) {
-		for (var i = offset; i =< limit; i++) {
-			auxList.push(death_penalty_stats[i]);
-			res.send(auxList);
-		}
-	} else {
+	// var limit = req.query.limit;
+	// var offset = req.query.offset;
+	// var auxList = []
+	// if (isNaN(offset)){
+	// 	offset = 0;
+	// }
+	// if (!isNaN(limit)) {
+	// 	for (var i = offset; i =< limit; i++) {
+	// 		auxList.push(death_penalty_stats[i]);
+	// 		res.send(auxList);
+	// 	}
+	// } else {
 	console.log("New GET of all resources.");
 	res.send(death_penalty_stats);
-	}
+	//}
 });
 
 app.get("/api/v1/death_penalty_stats/:country", (req,res) =>{
