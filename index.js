1d430c9a257b3b06fd82c86f1680e4c24300e5b3
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-04-04 23:34:23 +0200

    Implementing API methods for my source

--- index.js
+++ index.js
@@ -135,16 +135,13 @@ app.get("/api/v1/death_penalty_stats", (req,res) =>{
 
 		}
 	res.send(auxList);
-	// } else if(limit || offset){
-	// 	if (isNaN(offset)){
-	// 		offset = 0;
-	// 	}
-	// 	if (!isNaN(limit)) {
-	// 		for (var i = offset; i =< limit; i++) {
-	// 			auxList.push(death_penalty_stats[i]);
-	// 		}
-	// 	}
-	// res.send(auxList);
+	} else if(limit){
+		for (var i = 0; i <= limit; i++) {
+			auxList.push(death_penalty_stats[i]);
+		}
+		
+
+	res.send(auxList);
 	} else {
 	console.log("New GET of all resources.");
 	res.send(death_penalty_stats);
