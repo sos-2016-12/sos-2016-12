e19294f0937efd6dc6e3bf54c237a8d4d2fbae10
Victor <longjohnsilver@openmailbox.org>
2016-05-02 12:02:16 +0200

    1231

--- static/republican_stats/functions.js
+++ static/republican_stats/functions.js
@@ -18,7 +18,7 @@ $("body").ready(function (){
 	    request.done(function (data){
 	      console.log("Handling request (OK)");
 	      console.log("Data received: ");
-
+	      table(data);
 	    });
 
 	    request.always(function (jqXHR,status){
@@ -54,6 +54,7 @@ $("body").ready(function (){
 		    console.log("Handling request (OK)");
 		    console.log("Data received: ");
 		    alert('Created');
+		    table();
 		    
 
 		  });
@@ -101,6 +102,7 @@ $("body").ready(function (){
 		    console.log("Handling request (OK)");
 		    console.log("Data received: ");
 		    alert('Updated');
+		    table();
 		    
 
 		  });
@@ -134,20 +136,20 @@ $("body").ready(function (){
 	$("#searchButton").click(function(){
 	    console.log("Handling click");
 	    var apikey = $("#apikey").val();
-	    var country = $("#country").val();
-	    var year = $("#year").val();
+	    var countryS = $("#countryS").val();
+	    var yearS = $("#yearS").val();
 
-	    if(country){
+	    if(countryS){
 		  var request = $.ajax({
-		    url:"/api/v1/republican_stats/"+country+"/"+year+"?apikey="+apikey,
+		    url:"/api/v1/republican_stats/"+countryS+"/"+yearS+"?apikey="+apikey,
 		    type:"GET"
 		  });
-		}if(year){
+		}if(yearS){
 			var request = $.ajax({
-		    url:"/api/v1/republican_stats/"+year+"?apikey="+apikey,
+		    url:"/api/v1/republican_stats/"+yearS+"?apikey="+apikey,
 		    type:"GET"
 		  });
-		}if(!country && !year){
+		}if(!countryS && !yearS){
 			alert('What are you looking for?');
 		}
 
@@ -156,7 +158,7 @@ $("body").ready(function (){
 	  request.done(function (data){
 	    console.log("Handling request (OK)");
 	    console.log("Data received: ");
-	    
+	    table();
 
 
 	  });
@@ -173,6 +175,80 @@ $("body").ready(function (){
 
 	});
 
-
 });
+
+function table(){
+		var apikey = $("#apikey").val();
+		var countryS = $("#countryS").val();
+		/*var year = $("#year").val();
+		var gdppc = $("#gdppc").val();
+		var population = $("#population").val();*/
+
+		var url = "/api/v1/republican_stats";
+
+		if(countryS){
+		   	url = url+"/"+countryS+"?apikey="+apikey;;
+		}
+		else{
+			url = url +"?apikey="+apikey;
+		}
+
+		var request = $.ajax({
+		    url:url,
+		    type:"GET",
+		    contentType : "application/json"
+		});
+
+
+
+
+		request.done(function(data,status,jqXHR) {
+					var trHTML = '';
+                	$("idtable").find("tr:gt(0)").remove();
+       				$.each(data, function (i, item) {
+            
+            		trHTML += '<tr><td>' + data[i].country + '</td><td>' + data[i].year + '</td><td>' + data[i].gdppc + '</td><td>'
+            			+ data[i].population + '</td></tr>';
+       				});
+        
+       				$('#idtable').append(trHTML);
+       				console.log("tabla creada");
+		})
+		/*
+		request.done(function (data){
+		    console.log("Handling request (OK)");
+		    console.log("Data received: ");
+		    console.log(JSON.stringify(data));
+		    
+		    loadtable = $('<tbody></tbody>');
+		    for (i=0;i<data.length;i++){
+		      var clase = "data"+i;
+		      var row = $('<tr id='+clase+'></tr>').appendTo(loadtable);
+		      $('<td class="data"></td>').text(data[i].country).appendTo(row);
+		      $('<td class="data"></td>').text(data[i].year).appendTo(row);
+		      $('<td class="data"></td>').text(data[i].gdppc).appendTo(row);
+		      $('<td class="data"></td>').text(data[i].population).appendTo(row);
+		    }
+		    console.log("Table:"+loadtable.html());
+		    loadtable.appendTo("#idtable");
+		});
+		*/
+
+		request.always(function (jqXHR,status){
+		    if(status=="error"){
+		      console.log("Status: "+jqXHR.status);
+		      if(jqXHR.status==401)
+		      alert("You entered a wrong APIkey")
+		    }
+
+		});
+
+
+
+
+
+}
+
+
+
 
