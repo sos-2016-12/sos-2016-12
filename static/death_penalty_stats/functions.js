d210207b3a5dbd5ddce2cec8119b7b7a146bee10
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-05-03 02:09:35 +0200

    Configuring GUI of my resource

--- static/death_penalty_stats/functions.js
+++ static/death_penalty_stats/functions.js
@@ -130,12 +130,15 @@ $(document).ready(function(){
 
                 	id = 1;
 
-       				
+       				for (var i = data.length - 1; i >= 0; i--) {
+       					if (data[i].country == country)
+       						trHTML += '<tr id="'+id+'"><td>' + id +'</td><td>'+ data[i].country + '</td><td>' + data[i].abolition_year + '</td><td>' + data[i].for_all_crimes + '</td><td>'
+            				+ data[i].murder_rate_per_100k_people + '</td></tr>';
+       				 		id++;
+       				}
             
-            		trHTML += '<tr id="'+id+'"><td>' + id +'</td><td>'+ data[0].country + '</td><td>' + data[0].abolition_year + '</td><td>' + data[0].for_all_crimes + '</td><td>'
-            			+ data[0].murder_rate_per_100k_people + '</td></tr>';
-       				 id++;
-       				 
+            		
+       				
         
        				 $('#table').append(trHTML);
 					 $("#status").html(jqXHR.status);
