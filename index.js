e55a60e1c7130c4b8f281447f7ddfff0ee689a78
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-05-16 02:19:52 +0200

    Configuring Governify

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var governify = require("governify"); //apikey para governify = multiPlan_C2_sos
 governify.control(app,{
 	datastore : "http://datastore.governify.io/api/v6.1/",
 	namespace : "sos-2016-12-arr",
-	defaultPath : "/api/v1"
+	defaultPath : "https://sos-2016-12.herokuapp.com/api/v1"
 });
 /////////////// PROXY Ale///////////////////
 
