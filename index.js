885aa6a56976a209791f196eebd9b3a242014e04
Victor <longjohnsilver@openmailbox.org>
2016-05-29 00:39:07 +0200

    aoeu

--- index.js
+++ index.js
@@ -64,7 +64,6 @@ app.use(path,function(req,res){
          	res.sendStatus(503);//servicio no disponible
  	    }
     })).pipe(res);
-    console.log("datos"+res);
 });
 
 ///////////////////////////////////////
