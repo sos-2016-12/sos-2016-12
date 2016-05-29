dd8e827af721152e8481454c0337ac7c92d0eb25
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-05-30 00:06:17 +0200

    Setting up external APIs

--- index.js
+++ index.js
@@ -103,23 +103,23 @@ app.use(path,function(req,res){
 });
 
 
-var request3 = require("request");
+// var request3 = require("request");
 
-var path3 = '/rest/v1/all';
-var apiServerHost3 = 'https://ajayakv-rest-countries-v1.p.mashape.com';
+// var path3 = '/rest/v1/all';
+// var apiServerHost3 = 'https://ajayakv-rest-countries-v1.p.mashape.com';
 
-app.use(path3,function(req,res){
-	var url = apiServerHost2 + req.baseUrl + req.url;
-	console.log("Piped: "+ req.baseUrl + req.url);
- 	console.log("URL Accesed: "+ url);
+// app.use(path3,function(req,res){
+// 	var url = apiServerHost2 + req.baseUrl + req.url;
+// 	console.log("Piped: "+ req.baseUrl + req.url);
+//  	console.log("URL Accesed: "+ url);
  	
-	req.pipe(request2(url,(error,response,body)=>{
-      	if(error){
-	        console.error(error);
-         	res.sendStatus(503);//servicio no disponible
- 	    }
-    })).pipe(res);
-});
+// 	req.pipe(request2(url,(error,response,body)=>{
+//       	if(error){
+// 	        console.error(error);
+//          	res.sendStatus(503);//servicio no disponible
+//  	    }
+//     })).pipe(res);
+// });
 
 ///////////////////////////////////////
 
