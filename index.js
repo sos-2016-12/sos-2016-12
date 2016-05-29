642301a0f4309a530138b4af30888e61712bb27f
Alejandro Ruiz Rodríguez <ale_ruiz_rodriguez@hotmail.com>
2016-05-29 03:12:00 +0200

    Setting up external APIs

--- index.js
+++ index.js
@@ -221,7 +221,9 @@ app.get("/api/v1/death_penalty_stats/loadInitialData", (req,res) =>{
 	{ country : "Argentina", abolition_year : 1984, for_all_crimes : "no", murder_rate_per_100k_people : 7.0},
 	{ country : "France", abolition_year : 1981, for_all_crimes : "yes", murder_rate_per_100k_people : 1.2},
 	{ country : "Hungary", abolition_year : 1990, for_all_crimes : "yes", murder_rate_per_100k_people : 2.7},
+	{ country : "Russia", abolition_year : 1980, for_all_crimes : "no", murder_rate_per_100k_people : 9.0},
 	{ country : "SouthAfrica", abolition_year : 1997, for_all_crimes : "yes", murder_rate_per_100k_people : 31.9}];
+
 	console.log("12 elements initialized.");
 	res.sendStatus(201);
 });
